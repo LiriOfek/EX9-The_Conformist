aa049270d96d7a9ff64a9e2a4da1f809b81c3f9a
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-10 12:19:29 +0300

    add function that calculate the average of numbers in given array

--- Ex9-The_Conformist/main.c
+++ Ex9-The_Conformist/main.c
@@ -5,3 +5,77 @@ Purpose: This file get number of elements from user,
 		 get the elements, calculate the average of the numbers,
 		 and find the closest number to the average
 \********************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define INITIAL_INDEX (0)
+
+int* get_numbers_from_user_to_array(int* array_pointer,
+	int number_of_elements) {
+	/********************************************************\
+	* Function name - get_numbers_from_user_to_array
+	*
+	* Function Purpose - get numbers from user and
+	*					 fill the array with this numbers
+	*
+	* Parameters - INOUT int* array_pointe - the pointer to the array
+	*				that contain the numbers from the user
+	*			   IN int number_of_elements - the number of the elements
+	*				in the array
+	*
+	* Return Value - the pointer to the array that content the numbers
+	*					from the user
+	*
+	* Side Effects - this function change the array_pointer,
+	*				 such that it add to the array the numbers from user
+	*
+	* Semantics - this function get numbers from user
+	*			  and add them to the array
+	*
+	* Author - Liri
+	\********************************************************/
+	int index = INITIAL_INDEX;
+
+	printf("Enter the elements:\n");
+	for (index = INITIAL_INDEX; index < number_of_elements; ++index) {
+		scanf_s("%d", array_pointer + index);
+	}
+
+	return(array_pointer);
+}
+
+float average_of_numbers_in_array(int* array_pointer,
+	int number_of_elements) {
+	/********************************************************\
+	* Function name - average_of_numbers_in_array
+	*
+	* Function Purpose - calculate the average of the numbers in array
+	*
+	* Parameters - IN int* array_pointe - the pointer to the array
+	*				that contain the numbers from the user
+	*			   IN int number_of_elements - the number of the elements
+	*				in the array
+	*			   OUT float average of numbers in array
+	*
+	* Return Value - the average of the numbers that in the array
+	*
+	* Side Effects - this function has no side effects
+	*
+	* Semantics - this function calculate the average of the numbers
+	*			  that in the array
+	*
+	* Author - Liri
+	\********************************************************/
+	int index = INITIAL_INDEX;
+	int summary = INITIAL_INDEX;
+	float average = INITIAL_INDEX;
+
+	for (index = INITIAL_INDEX; index < number_of_elements; ++index)
+	{
+		summary += *(array_pointer + index);
+	}
+	average = (float)summary / number_of_elements;
+	return average;
+}
