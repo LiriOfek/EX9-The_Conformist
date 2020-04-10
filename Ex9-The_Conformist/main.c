ad5893b8fe154dd3e84f89300ad05251c2c116a3
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-10 12:16:52 +0300

    add main file with documentation

---
+++ Ex9-The_Conformist/main.c
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (10/4/2020)
+Purpose: This file get number of elements from user,
+		 get the elements, calculate the average of the numbers,
+		 and find the closest number to the average
+\********************************************************/
